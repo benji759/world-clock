8e25574c3b68a77b670dad929b220a5b12936664
benjikrystal <benmzh@gmail.com>
2024-10-23 09:19:38 +0100

    Handle loading and errors

--- world-clock-react-app/src/App.js
+++ world-clock-react-app/src/App.js
@@ -1,6 +1,18 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  if (loading) {
+    return <div className="App">Loading...</div>;
+  }
+
+  if (error) {
+    return <div className="App">Error: {error}</div>;
+  }
+
   return (
     <div className="App">
     </div>
